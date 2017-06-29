16480851930d67edbfcb659191549b8bb6cbe068
Tomoya Koyanagi <tomk79@gmail.com>
2017-06-29 13:05:28 +0900

    パンくずのパスを解決するようになった (+)
    - contents.csv の先頭の A1 列が ""id"" のように読み込まれる問題があった。CSVファイルの先頭行をごにょごにょしたら何故か直ったが、もしかしたら先頭に何らかの見えない文字？ があったのかもしれない。

--- php/export/sitemap.php
+++ php/export/sitemap.php
@@ -21,42 +21,10 @@ class export_sitemap{
 	 * @return boolean 実行結果の真偽
 	 */
 	public function export( $path_output ){
-		$column_define_contents = array(
-			"id",
-			"name",
-			"plugin",
-			"type",
-			"entity_id",
-			"url",
-			"site_id",
-			"alias_id",
-			"main_site_content_id",
-			"parent_id",
-			"lft",
-			"rght",
-			"level",
-			"title",
-			"description",
-			"eyecatch",
-			"author_id",
-			"layout_template",
-			"status",
-			"publish_begin",
-			"publish_end",
-			"self_status",
-			"self_publish_begin",
-			"self_publish_end",
-			"exclude_search",
-			"created_date",
-			"modified_date",
-			"site_root",
-			"deleted_date",
-			"deleted",
-			"exclude_menu",
-			"blank_link",
-			"created",
-			"modified",
-		);
+		$path_contents_csv = $path_output.'exports/core/contents.csv';
+		$contents_csv = $this->core->fs()->read_csv( $path_contents_csv, array('charset'=>'utf-8') );
+		$column_define_contents = $contents_csv[0];
+
 		$row_template_contents = array();
 		foreach($column_define_contents as $column_name){
 			$row_template_contents[$column_name] = null;
@@ -70,23 +38,56 @@ class export_sitemap{
 		}
 		// var_dump($sitemap);
 
+		// contents.csv 上でのIDと、Pickles 2 の page_id の紐付けテーブル
+		$relay_page_id2contents_id_table = array();
+		$relay_path2page_id_table = array();
+		$id = 0;
+		foreach($sitemap as $page_info){
+			$id++;
+			$relay_page_id2contents_id_table[$page_info->id] = $id;
+			$relay_path2page_id_table[$page_info->path] = $page_info->id;
+		}
+
+		// contents.csv を生成
 		$csv = array();
 		array_push($csv, $column_define_contents );
 		$id = 0;
 		foreach($sitemap as $page_info){
 			$id++;
+			$relay_page_id2contents_id_table[$page_info->id] = $id;
 			$row = $row_template_contents;
-			$row['id'] = $id;
+			$row['id'] = $relay_page_id2contents_id_table[$page_info->id];
 			$row['name'] = $page_info->id;
 			$row['plugin'] = 'Core';
 			$row['type'] = 'Page';
 			$row['url'] = $page_info->path;
 			$row['site_id'] = '0';
 			$row['title'] = $page_info->title;
+			$row['description'] = $page_info->description;
+
+			// コンテンツのID
+			// pages.csv のIDに紐づく
+			$row['entity_id'] = '1';
+
+			// パンくずの階層構造
+			if( !strlen($page_info->id) ){
+				$row['parent_id'] = '';
+			}elseif( !strlen($page_info->logical_path) ){
+				$row['parent_id'] = '1';
+			}else{
+				$breadcrumbs = explode('>', trim($page_info->logical_path));
+				$parent_page_id = $breadcrumbs[count($breadcrumbs)-1];
+				if( strlen(@$relay_path2page_id_table[$parent_page_id]) ){
+					$parent_page_id = $relay_path2page_id_table[$parent_page_id];
+				}
+				$row['parent_id'] = $relay_page_id2contents_id_table[$parent_page_id];
+			}
+
+			// 行完成
 			array_push($csv, $row );
 		}
 		$src_csv = $this->core->fs()->mk_csv( $csv );
-		$this->core->fs()->save_file( $path_output.'exports/core/contents.csv', $src_csv );
+		$this->core->fs()->save_file( $path_contents_csv, $src_csv );
 
 		return true;
 	}
